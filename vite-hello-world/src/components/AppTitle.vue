0bd13921fbb9cc2430823645f353738a0f82f65c
ChiaraS97 <chiara.sucato97@gmail.com>
2023-04-12 14:25:01 +0200

    create component "AppTitle"

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                title: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
