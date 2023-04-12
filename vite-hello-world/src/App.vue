8d2e8000e802336dd1a17030bec2eff3a7f0e023
ChiaraS97 <chiara.sucato97@gmail.com>
2023-04-12 14:34:58 +0200

    add AppHero (bonus)

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <AppTitle/>
+  <AppHero/>
 </template>
 
 <script>
   import AppTitle from './components/AppTitle.vue';
+  import AppHero from './components/AppHero.vue';
   export default {
     name: 'App',
-    components: { AppTitle }
+    components: { AppTitle, AppHero }
 }
 </script>
 
---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,15 @@
+:<template>
+    <div class="jumbo">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
